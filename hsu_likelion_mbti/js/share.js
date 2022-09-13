ae6c1ced3fdfa0fb6cab456c52871c8baaf52ffd
jaminleee <jamin_00@naver.com>
2022-09-14 05:59:15 +0900

    share.js 생성

---
+++ hsu_likelion_mbti/js/share.js
@@ -0,0 +1,42 @@
+
+function shareFacebook() {
+    var sendUrl = "https://hsu-likelion-mbti.netlify.app/"; // 전달할 URL
+    window.open("http://www.facebook.com/sharer/sharer.php?u=" + sendUrl);
+}
+
+function sendLink() {
+    Kakao.Link.sendDefault({
+        objectType: 'feed',
+        content: {
+            title: '나와 닮은 상상부기 프렌즈는?',
+            description: '#MBTI #상상부기 #한성냥이 #꼬꼬꾸꾸 #상찌 # #HSU #멋사 #운영진수고했다',
+            imageUrl: './img/thumbnail.png',
+            link: {
+                mobileWebUrl: 'https://hsu-likelion-mbti.netlify.app/',
+                webUrl: 'https://hsu-likelion-mbti.netlify.app/',
+            },
+        },
+        buttons: [
+            {
+                title: '테스트 하러가기!!',
+                link: {
+                    mobileWebUrl: 'https://hsu-likelion-mbti.netlify.app/',
+                    webUrl: 'https://hsu-likelion-mbti.netlify.app/',
+                },
+            },
+        ],
+    })
+}
+
+function clip() {
+
+    var url = 'https://hsu-likelion-mbti.netlify.app/';
+    var textarea = document.createElement("textarea");
+    document.body.appendChild(textarea);
+    /* url = window.document.location.href;*/
+    textarea.value = url;
+    textarea.select();
+    document.execCommand("copy");
+    document.body.removeChild(textarea);
+    alert("클립보드에 복사되었습니다.")
+}
